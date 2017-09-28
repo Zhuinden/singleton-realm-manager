7a88fcf1de3d193fb5d7b754c6f6a55f8ae121f5
zhuinden <zhuinden@gmail.com>
2017-09-28 15:50:49 +0200

    WIP auto-migration

---
+++ realm-manager-example/src/main/java/com/zhuinden/realmmanagerexample/Dog.java
@@ -0,0 +1,24 @@
+package com.zhuinden.realmmanagerexample;
+
+//import com.zhuinden.realmmanagerexample.automigration.AutoMigration;
+//
+//import io.realm.FieldAttribute;
+//import io.realm.RealmObject;
+//import io.realm.annotations.Index;
+//import io.realm.annotations.Required;
+
+/**
+ * Created by Owner on 2017. 09. 28..
+ */
+//
+//public class Dog extends RealmObject {
+//    @Index
+//    @AutoMigration.MigratedField(fieldAttributes = {FieldAttribute.INDEXED})
+//    private String name;
+//
+//    @Required
+//    @AutoMigration.MigratedField(fieldAttributes = {FieldAttribute.REQUIRED})
+//    private String ownerName;
+//
+//    private Cat cat;
+//}
